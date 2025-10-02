cba3ad95df5eb2c8fc7fd48e3277fdc12c5e93b2
Dalvir Singh <dalvirsingh@dss-MacBook-Air.local>
2025-10-02 16:42:05 +0530

    por change

--- server/vite.ts
+++ server/vite.ts
@@ -69,7 +69,6 @@ export async function setupVite(app: Express, server: Server) {
 
 export function serveStatic(app: Express) {
   const distPath = path.resolve(import.meta.dirname, "public");
-
   if (!fs.existsSync(distPath)) {
     throw new Error(
       `Could not find the build directory: ${distPath}, make sure to build the client first`,
